277e58008e2f3d63117b4f9cf81497023245dca8
Mr. Llanes <llanesm@gmail.com>
2021-03-05 01:24:20 -0600

    Creation and initial coding of index.js.

---
+++ index.js
@@ -0,0 +1,82 @@
+// Dependencies
+const database = require("./db/schema");
+const connection = require("./config/connection");
+const inquirer = require("inquirer");
+
+connection.connect((err) => {
+	if (err) throw err;
+	console.log(`connected as id ${connection.threadId}`);
+	mainMenu();
+});
+
+const mainMenu = () => {
+	inquirer
+		.prompt({
+			name: "firstAction",
+			type: "rawlist",
+			message: "Please make a selection below",
+			choices: [
+				"View all Employees",
+				"View all Departments",
+				"View all roles",
+				"Add an Employee",
+				"Add a Department",
+				"Add a role",
+				"Update Employee Roles",
+				"Exit Program",
+			],
+		})
+		.then((answer) => {
+			switch (answer.action) {
+				case "View all Employees":
+					viewEmployees();
+					break;
+
+				case "View all Departments":
+					viewDepartments();
+					break;
+
+				case "View all roles":
+					viewRoles();
+					break;
+
+				case "Add an Employee":
+					addEmployee();
+					break;
+
+				case "Add a Department":
+					addDepartment();
+					break;
+
+				case "Add a role":
+					addRole();
+					break;
+
+				case "Update Employee Roles":
+					updateRoles();
+					break;
+
+				case "Exit Program":
+					connection.end();
+					break;
+
+				default:
+					console.log(`Invalid action: ${answer.action}`);
+					break;
+			}
+		});
+};
+
+const viewEmployees = () => {};
+
+const viewDepartments = () => {};
+
+const viewRoles = () => {};
+
+const addEmployee = () => {};
+
+const addDepartment = () => {};
+
+const addRole = () => {};
+
+const updateRoles = () => {};
